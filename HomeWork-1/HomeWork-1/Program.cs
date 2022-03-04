a4547181bfec482b9806a25bde51907c27f9cfda
Olga Mukharskaya <olgamukharskaya@coherentsolutions.com>
2022-03-04 11:28:38 +0300

    changed to constants

--- HomeWork-1/HomeWork-1/Program.cs
+++ HomeWork-1/HomeWork-1/Program.cs
@@ -7,15 +7,14 @@ namespace C_Training_Test_01
         static void Main(string[] args)
         {
 
-
             Console.WriteLine("Max and min values of byte are {0} -- {1}", byte.MaxValue, byte.MinValue);
-            Console.WriteLine("Max and min values of short are {0} -- {1}", maxShortValue, minShortValue);
-            Console.WriteLine("Max and min values of int are {0} -- {1}", maxIntValue, minIntValue);
-            Console.WriteLine("Max and min values of long are {0} -- {1}", maxLongValue, minLongValue);
-            Console.WriteLine("Max and min values of float are {0} -- {1}", maxFloatValue, minFloatValue);
-            Console.WriteLine("Max and min values of double are {0} -- {1}", maxDoubleValue, minDoubleValue);
-            Console.WriteLine("Max and min values of uint are {0} -- {1}", maxuIntValue, minuIntValue);
-            Console.WriteLine("Max and min values of ulong are {0} -- {1}", maxuLongValue, minuLongValue);
+            Console.WriteLine("Max and min values of short are {0} -- {1}", short.MaxValue, short.MinValue);
+            Console.WriteLine("Max and min values of int are {0} -- {1}", int.MaxValue, int.MinValue);
+            Console.WriteLine("Max and min values of long are {0} -- {1}", long.MaxValue, long.MinValue);
+            Console.WriteLine("Max and min values of float are {0} -- {1}", float.MaxValue, float.MinValue);
+            Console.WriteLine("Max and min values of double are {0} -- {1}", double.MaxValue, double.MinValue);
+            Console.WriteLine("Max and min values of uint are {0} -- {1}", int.MaxValue, int.MinValue);
+            Console.WriteLine("Max and min values of ulong are {0} -- {1}", long.MaxValue, long.MinValue);
 
             // [10.02.2022 13:08]
             //Задача- вывести в консоль максимальные и минимальные допустимые значения для типов данных byte, short, int, long,  float, double, uint, ulong.
