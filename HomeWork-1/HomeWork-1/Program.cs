c37637e11e1eba0816e7487d95dacc060975b1f5
Olga Mukharskaya <olgamukharskaya@coherentsolutions.com>
2022-03-04 11:12:07 +0300

    HomeWork 1 is done

---
+++ HomeWork-1/HomeWork-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_Training_Test_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Max and min values of byte are {0} -- {1}", byte.MaxValue, minByteValue);
+            Console.WriteLine("Max and min values of short are {0} -- {1}", maxShortValue, minShortValue);
+            Console.WriteLine("Max and min values of int are {0} -- {1}", maxIntValue, minIntValue);
+            Console.WriteLine("Max and min values of long are {0} -- {1}", maxLongValue, minLongValue);
+            Console.WriteLine("Max and min values of float are {0} -- {1}", maxFloatValue, minFloatValue);
+            Console.WriteLine("Max and min values of double are {0} -- {1}", maxDoubleValue, minDoubleValue);
+            Console.WriteLine("Max and min values of uint are {0} -- {1}", maxuIntValue, minuIntValue);
+            Console.WriteLine("Max and min values of ulong are {0} -- {1}", maxuLongValue, minuLongValue);
+
+            // [10.02.2022 13:08]
+            //Задача- вывести в консоль максимальные и минимальные допустимые значения для типов данных byte, short, int, long,  float, double, uint, ulong.
+
+        }
+    }
+}
